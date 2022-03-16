a7ed1311fa1c79dd2ae932687948643d77c51be9
Kian <75130117+Kian1010@users.noreply.github.com>
2022-03-16 13:00:23 +0200

    Delete 2 - string manipulation directory

--- 2 - string manipulation/main.py
+++
@@ -1,8 +0,0 @@
-print("Day 1 - String Manipulation")
-print("String Concatenation is done with the \"+\" sign.")
-print('e.g. print("Hello " + "world")')
-print("New lines can be created with a backslash and n.")
-
-print("")
-# use of '\n' to break the line
-print("Hello world\nHello world")
