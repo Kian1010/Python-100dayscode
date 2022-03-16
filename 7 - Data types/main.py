94c9bb40a0c1ef614a514780053afebc21a8f144
Kian <75130117+Kian1010@users.noreply.github.com>
2022-03-16 13:01:58 +0200

    Delete 7 - Data types directory

--- 7 - Data types/main.py
+++
@@ -1,7 +0,0 @@
-# gets a double digit number then adds the first and second digits together
-two_digit_number = input("Type a two digit number: ")
-
-num1 = int(two_digit_number[0])
-num2 = int(two_digit_number[1])
-
-print(num1 + num2)
